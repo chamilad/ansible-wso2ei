a266bf47eca6e2421017f1e7674206f70ea19412
Chamila de Alwis <chamila@apache.org>
2017-09-25 07:47:36 -0400

    Fix server start task

--- test_playbook.sh
+++ test_playbook.sh
@@ -9,15 +9,9 @@ echo "Testing host reachability..."
 ansible test -m ping -i hosts/test.ini --connection=local
 
 echo "Checking syntax...."
-ansible-playbook playbooks/wso2ei-6.1.1/ubuntu.yaml --syntax-check -i hosts/test.ini
+ansible-playbook playbooks/wso2ei-6.1.1/ubuntu.yaml --syntax-check -i hosts/test.ini --connection=local
 
 if [ $? == 0 ]; then
   echo "Running Playbook..."
-  ansible-playbook playbooks/wso2ei-6.1.1/ubuntu.yaml --connection=local -i hosts/test.ini
-
-  # TODO: Temp, until running this in ansible is figured out
-  echo "Running WSO2 server..."
-  pushd $tmp_path/wso2ei-6.1.1/bin > /dev/null 2>&1
-    bash business-process.sh
-  popd > /dev/null 2>&1
+  ansible-playbook playbooks/wso2ei-6.1.1/ubuntu.yaml --connection=local -i hosts/test.ini --connection=local
 fi
